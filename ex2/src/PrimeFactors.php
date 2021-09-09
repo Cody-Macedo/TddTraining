b7dad2d8fd869936c6d918eb3c55d058744731b5
Cody-Macedo <macedo.cody@gmail.com>
2021-09-09 13:44:47 +0200

    split in directory exercice

--- ex2/src/PrimeFactors.php
+++ ex2/src/PrimeFactors.php
@@ -16,6 +16,7 @@ final class PrimeFactors
         } else {
             return [];
         }
+        return [];
     }
 
 }
---
+++ ex3/tests/ParenthesisTest.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class ParenthesisTest extends TestCase
+{
+    public function testGoodParenthesis()
+    {
+        $this->assertEquals(true, Parenthesis::isGoodParenthesis("(foo(bar))"));
+        $this->assertEquals(true, Parenthesis::isGoodParenthesis(""));
+        $this->assertEquals(true, Parenthesis::isGoodParenthesis("()"));
+        $this->assertEquals(true, Parenthesis::isGoodParenthesis("foo"));
+    }
+
+    public function testNotGoodParenthesis()
+    {
+        $this->assertEquals(false, Parenthesis::isGoodParenthesis("((foo)"));
+        $this->assertEquals(false, Parenthesis::isGoodParenthesis("())"));
+        $this->assertEquals(false, Parenthesis::isGoodParenthesis(")(()"));
+    }
+
+}

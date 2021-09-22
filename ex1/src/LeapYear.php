ad3a8050b7ae7966103f74e64de072be00494643
Cody-Macedo <macedo.cody@gmail.com>
2021-09-22 14:24:08 +0200

    add regex

--- ex1/src/LeapYear.php
+++ ex1/src/LeapYear.php
@@ -16,4 +16,43 @@ final class LeapYear
         return false;
 
     }
+
+    public static function looksValid($date): bool
+    {
+        $regex = '/[0-9]{4}-[0-9]{2}-[0-9]{2}/';
+        $value = preg_match($regex, $date);
+        if ($value) {
+            return true;
+        } else {
+            return false;
+        }
+
+//        var_dump($matches);
+//        if (!empty($matches)) {
+//            $dateArray = explode("-", $date);
+//
+//            if ((int)$dateArray[0] > 1500){
+//                if((int)$dateArray[1] < 13){
+//
+//                }else{
+//                    return false;
+//                }
+//            }else{
+//                return false;
+//            }
+//        } else {
+//            return false;
+//        }
+//        return true;
+    }
+
+    public static function urlAllImages($url): array
+    {
+
+        $regex = '/^<img.*src="(.+?)".*">/';
+        preg_match($regex, $url, $matches);
+        var_dump($matches);
+        return $matches;
+
+    }
 }
--- ex1/tests/LeapYearTest.php
+++ ex1/tests/LeapYearTest.php
@@ -24,5 +24,18 @@ final class LeapYearTest extends TestCase
         $this->assertEquals(false, LeapYear::isLeap(1900), "1900 ne devrait pas être bissextile");
     }
 
+    public function testLooksValid()
+    {
+        $this->assertEquals(true, LeapYear::looksValid('2021-09-22'), "not good");
+        $this->assertEquals(true, LeapYear::looksValid('1975-12-03'), "not good");
+        $this->assertEquals(false, LeapYear::looksValid('2021-9-22'), "not good");
+        $this->assertEquals(true, LeapYear::looksValid('1308-12-03'), "not good");
+    }
+
+    public function testUrlAllImages()
+    {
+        $this->assertEquals(true, LeapYear::urlAllImages('https://www.digital-campus.fr/ecole/paris'), "not good");
+
+    }
 
 }
